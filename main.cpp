20e8c7a9915293d3f32fe6efde9bc74af7486a3e
Mattiasiri01 <100609727+Mattiasiri01@users.noreply.github.com>
2022-05-07 12:19:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
+int main{
+  int dim=0;
+  cin>>dim;
+  float numeri[dim];
+  for(int i=0; i<dim; i++){
+    cin>>numeri[i];
+  }
+  
+  cout<<max(numeri, d);
   return 0;
 }
